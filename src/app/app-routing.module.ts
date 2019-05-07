4965e0fad34eae6be70c732586034b5a324ef40a
vyeung <vl.yeung@yahoo.com>
2019-05-07 11:36:31 -0400

    add AdminLayout, Admin, and Books cmpt routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
+import { AdminLayoutComponent } from "./admin-layout/admin-layout.component";
+import { AdminComponent } from "./admin/admin.component";
+import { BooksComponent } from "./admin/books/books.component";
 
 const routes: Routes = [
   {
@@ -16,6 +19,20 @@ const routes: Routes = [
       {
         path: "adminLogin",
         component: LoginComponent
+      },
+      {
+        path: "admin",
+        component: AdminLayoutComponent,
+        children: [
+          {
+            path: "dashboard",
+            component: AdminComponent
+          },
+          {
+            path: "books",
+            component: BooksComponent
+          }
+        ]
       }
     ]
   }
