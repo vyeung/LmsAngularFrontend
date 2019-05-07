2f718e78310c982e2e9da9b25817c1a6ee808a12
vyeung <vl.yeung@yahoo.com>
2019-05-06 22:15:27 -0400

    add LoginComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: "",
         component: HomeComponent 
+      },
+      {
+        path: "adminLogin",
+        component: LoginComponent
       }
     ]
   }
