6d373eb19ae4f036ee8b68dd70f94e6041e8e17f
vyeung <vl.yeung@yahoo.com>
2019-05-09 14:53:09 -0400

    finish books cmpt

--- src/app/admin/books/books.component.ts
+++ src/app/admin/books/books.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from "../../services/admin.service";
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css', '../admin.component.css']
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  books: any;
+
+  constructor(private adminService: AdminService) {}
 
   ngOnInit() {
+    this.getAllBooks();
   }
 
+  getAllBooks() {
+    this.adminService.getAllBooks().subscribe(res => {
+      this.books = res;
+    })
+  }
+
+  deleteBookHandler(id) {
+    this.adminService.deleteBook(id).subscribe(res => {
+      this.getAllBooks();
+    });
+  }
 }
