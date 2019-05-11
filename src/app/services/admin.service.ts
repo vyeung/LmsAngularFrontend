b93efb19ad71a163e1e45298439d726318c34ec4
vyeung <vl.yeung@yahoo.com>
2019-05-11 13:45:48 -0400

    cleanup admin.service

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -8,11 +8,11 @@ import { environment } from '../../environments/environment';
 export class AdminService {
 
   constructor(
-    private http: HttpClient,
-  ) { }
+    private http: HttpClient
+  ) {}
 
-// book methods 
 
+  /* Book methods */
   getAllBooks() {
     return this.http.get(`${environment.api_endpoint}/books`);
   }
@@ -29,102 +29,88 @@ export class AdminService {
     return this.http.delete(`${environment.api_endpoint}/book/${id}`);
   }
 
-// publisher methods 
+
+  /* Publisher methods */
+  getAllPublishers(query) {
+    return this.http.get(`${environment.api_endpoint}/publishers`+ query);
+  }
+  getPublisher(id) {
+    return this.http.get(`${environment.api_endpoint}/publisher/${id}`);
+  }
+  addPublisher(publisher) {
+    return this.http.post(`${environment.api_endpoint}/publisher`, publisher);
+  }
+  updatePublisher(publisher, id) {
+    return this.http.put(`${environment.api_endpoint}/publisher/${id}`, publisher);
+  }
+  deletePublisher(id) {
+    return this.http.delete(`${environment.api_endpoint}/publisher/${id}`);
+  }
 
 
-getAllPublishers(query) {
-  return this.http.get(`${environment.api_endpoint}/publishers`+ query);
-}
-getPublisher(id) {
-  return this.http.get(`${environment.api_endpoint}/publisher/${id}`);
-}
-addPublisher(publisher) {
-  return this.http.post(`${environment.api_endpoint}/publisher`, publisher);
-}
-updatePublisher(publisher, id) {
-  return this.http.put(`${environment.api_endpoint}/publisher/${id}`, publisher);
-}
-deletePublisher(id) {
-  return this.http.delete(`${environment.api_endpoint}/publisher/${id}`);
-}
+  /* Branch methods */
+  getAllBranches(query) {
+    return this.http.get(`${environment.api_endpoint}/libraryBranches`+ query);
+  }
+  getBranch(id) {
+    return this.http.get(`${environment.api_endpoint}/libraryBranch/${id}`);
+  }
+  addBranch(branch) {
+    return this.http.post(`${environment.api_endpoint}/libraryBranch`, branch);
+  }
+  updateBranch(branch,id) {
+    return this.http.put(`${environment.api_endpoint}/libraryBranch/${id}`, branch);
+  }
+  deleteBranch(id) {
+    return this.http.delete(`${environment.api_endpoint}/libraryBranch/${id}`);
+  }
 
 
-// branch methodes 
+  /* Due Date methods */ 
+  getAllLoans(query) {
+    return this.http.get(`${environment.api_endpoint}/bookLoans`+ query);
+  }
+  getAllLoansDetails(query) {
+    return this.http.get(`${environment.api_endpoint}/bookLoansAsDTO`+ query);
+  }
+  updateBookLoanDueDate(loan,bookId,branchId,cardNo) {
+    return this.http.put(
+    `${environment.api_endpoint}/bookLoans/bookId/${bookId}/branchId/${branchId}/cardNo/${cardNo}`, loan);
+  }
 
 
-getAllBranches(query) {
-  return this.http.get(`${environment.api_endpoint}/libraryBranches`+ query);
-}
-getBranch(id) {
-  return this.http.get(`${environment.api_endpoint}/libraryBranch/${id}`);
-}
-addBranch(branch) {
-  return this.http.post(`${environment.api_endpoint}/libraryBranch`, branch);
-}
-updateBranch(branch,id) {
-  return this.http.put(`${environment.api_endpoint}/libraryBranch/${id}`, branch);
-}
-deleteBranch(id) {
-  return this.http.delete(`${environment.api_endpoint}/libraryBranch/${id}`);
-}
+  /* Author methods */
+  getAllAuthors() {
+    return this.http.get(`${environment.api_endpoint}/authors`);
+  }
+  getAuthor(id){
+    return this.http.get(`${environment.api_endpoint}/author/${id}`);
+  }
+  addAuthor(author){
+    return this.http.post(`${environment.api_endpoint}/author`, author);
+  }
+  deleteAuthor(id) {
+    return this.http.delete(`${environment.api_endpoint}/author/${id}`);
+  }
+  editAuthor(id, author){
+    return this.http.put(`${environment.api_endpoint}/author/${id}`, author);
+  }
 
 
-// due date methods 
-
-getAllLoans(query) {
-  return this.http.get(`${environment.api_endpoint}/bookLoans`+ query);
-}
-
-getAllLoansDetails(query) {
-  return this.http.get(`${environment.api_endpoint}/bookLoansAsDTO`+ query);
-}
-
-updateBookLoanDueDate(loan,bookId,branchId,cardNo) {
-  return this.http.put(
-  `${environment.api_endpoint}/bookLoans/bookId/${bookId}/branchId/${branchId}/cardNo/${cardNo}`, loan);
-}
-
-
-/* Authors */
-getAllAuthors() {
-  return this.http.get(`${environment.api_endpoint}/authors`);
-}
-
-getAuthor(id){
-  return this.http.get(`${environment.api_endpoint}/author/${id}`);
-}
-
-addAuthor(author){
-  return this.http.post(`${environment.api_endpoint}/author`, author);
-}
-
-deleteAuthor(id) {
-  return this.http.delete(`${environment.api_endpoint}/author/${id}`);
-}
-editAuthor(id, author){
-  return this.http.put(`${environment.api_endpoint}/author/${id}`, author);
-}
-
-/* Borrowers */
-getAllBorrowers() {
-  return this.http.get(`${environment.api_endpoint}/borrowers`);
-}
-
-getBorrower(id){
-  return this.http.get(`${environment.api_endpoint}/borrower/${id}`);
-}
-
-addBorrower(borrower){
-  return this.http.post(`${environment.api_endpoint}/borrower`, borrower);
-}
-
-deleteBorrower(id) {
-  return this.http.delete(`${environment.api_endpoint}/borrower/${id}`);
-}
-editBorrower(id, borrower){
-  return this.http.put(`${environment.api_endpoint}/borrower/${id}`, borrower);
-}
-
-
-
+  /* Borrower methods */
+  getAllBorrowers() {
+    return this.http.get(`${environment.api_endpoint}/borrowers`);
+  }
+  getBorrower(id){
+    return this.http.get(`${environment.api_endpoint}/borrower/${id}`);
+  }
+  addBorrower(borrower){
+    return this.http.post(`${environment.api_endpoint}/borrower`, borrower);
+  }
+  deleteBorrower(id) {
+    return this.http.delete(`${environment.api_endpoint}/borrower/${id}`);
+  }
+  editBorrower(id, borrower){
+    return this.http.put(`${environment.api_endpoint}/borrower/${id}`, borrower);
+  }
 }
